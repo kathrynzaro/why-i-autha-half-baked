c273c86311bb6832bb1e2edf19d0a9275293ae93
kathrynzaro <zarokathryn@gmail.com>
2022-05-16 14:55:01 -0700

    fixing PR errors

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 import { redirectIfLoggedIn, signInUser, signupUser } from './fetch-utils.js';
 
 const signInForm = document.getElementById('sign-in');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-in-password');
 
 const signUpForm = document.getElementById('sign-up');
-const signUpEmail = document.getElementById('sign-up-email');
-const signUpPassword = document.getElementById('sign-up-password');
 
 // Wire up sign in and sign up forms to supabase
 redirectIfLoggedIn();
